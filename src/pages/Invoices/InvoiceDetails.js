a4aae22726e23a21d1380c5c2a47fc68491c0f7f
Nguyendungx <dung0916149123@gmail.com>
2023-11-16 19:38:18 +0700

    Thêm xóa dịch vụ, phụ phí đã đặt trong hóa đơn

--- src/pages/Invoices/InvoiceDetails.js
+++ src/pages/Invoices/InvoiceDetails.js
@@ -1,71 +1,116 @@
 import React, { useState, useEffect } from 'react';
-import './InvoiceDetails.css'
+import './InvoiceDetails.css';
+import DeleteClientForm from '../Clients/DeleteClients.js';
+import { AiTwotoneDelete } from 'react-icons/ai';
 
 function InvoiceDetails(props) {
-
   const [dataServices, setDataServices] = useState([]);
-const [dataSurcharges, setDataSurcharges] = useState([]);
+  const [dataSurcharges, setDataSurcharges] = useState([]);
 
-const apiDichVus = 'https://service-hotelmanagement-dev.azurewebsites.net/api/ThemDichVus';
-const apiSurcharges = 'https://service-hotelmanagement-dev.azurewebsites.net/api/ThemPhuPhis'; // Thay đổi URL của API thứ hai tại đây
+  const apiDichVus = 'https://service-hotelmanagement-dev.azurewebsites.net/api/ThemDichVus';
+  const apiSurcharges = 'https://service-hotelmanagement-dev.azurewebsites.net/api/ThemPhuPhis';
 
-const fetchDataWithAuthorization = async (api, setData) => {
-  const maSKThuePhong = props.editData.maSKThuePhong;
-  try {
-    const response = await fetch(`${api}?MaSKDP=${maSKThuePhong}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
+  const fetchDataWithAuthorization = async (api, setData) => {
+    const maSKThuePhong = props.editData.maSKThuePhong;
+    try {
+      const response = await fetch(`${api}?MaSKDP=${maSKThuePhong}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
 
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.status}`);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setData(data.data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
 
-    const data = await response.json();
-    setData(data.data);
-    console.log(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
+  //=============DELETE=================
+  const [serviceToDelete, setServiceToDelete] = useState(null);
+  const [deleteService, setDeleteServices] = useState(false);
 
-const fetchData = async () => {
-  try {
-     await Promise.all([
-      fetchDataWithAuthorization(apiDichVus, setDataServices),
-      fetchDataWithAuthorization(apiSurcharges, setDataSurcharges),
-    ]);
-    // Do something with both responses if needed
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
+  const ShowDeleteService = () => setDeleteServices(!deleteService);
 
-// Call the function to fetch data
+  const handleDeleteClick = (service) => {
+    setServiceToDelete(service);
+    ShowDeleteService(true);
+  };
+
+  const handleCancelDelete = () => {
+    ShowDeleteService(false);
+  };
+
+  const handleRemoveClick = (type) => {
+    if (serviceToDelete) {
+      const maSK = serviceToDelete.maSK;
+      const apiUrl = type === 'services' ? apiDichVus : apiSurcharges;
+      
+      const data = type === 'services' ? dataServices : dataSurcharges;
+      const setData = type === 'services' ? setDataServices : setDataSurcharges;
+
+      const indexToRemove = data.findIndex((item) => item.maSK === maSK);
+
+      if (indexToRemove !== -1) {
+        fetch(`${apiUrl}/${maSK}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`Network response was not ok: ${response.status}`);
+            }
+
+            const newData = [...data];
+            newData.splice(indexToRemove, 1);
+
+            setData(newData);
+          })
+          .catch((error) => {
+            console.error('Error deleting data:', error);
+          });
+
+        ShowDeleteService(false);
+      }
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      await Promise.all([
+        fetchDataWithAuthorization(apiDichVus, setDataServices),
+        fetchDataWithAuthorization(apiSurcharges, setDataSurcharges),
+      ]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  
-  
+  const handleCancelClick = () => {
+    props.onCancel();
+  };
 
-
-
-const handleCancelClick = () => {
-  // Gọi hàm callback để thông báo cho component cha
-  props.onCancel();
-};
   return (
     <div>
       <div className="formInvoiceDetails">
         <div className="overlay">
           <div className="form-container">
             <div className="tableServices">
-                <lable><strong>Danh sách dịch vụ</strong></lable>
+              <label><strong>Danh sách dịch vụ</strong></label>
               <table>
                 <thead>
                   <tr>
@@ -85,17 +130,17 @@ const handleCancelClick = () => {
                       <td>{d.maNV}</td>
                       <td>{d.soLuong}</td>
                       <td>{d.tongTien}</td>
-                      <td>{d.thoiGian }</td>
+                      <td>{d.thoiGian}</td>
+                      <td id='removeData' onClick={() => handleDeleteClick(d)}><AiTwotoneDelete /></td>
                     </tr>
                   ))}
                 </tbody>
-              </table>       
-              
+              </table>
             </div>
 
             <div className="tableSurcharges">
-            <lable><strong>Danh sách phụ phí</strong></lable>
-            <table>
+              <label><strong>Danh sách phụ phí</strong></label>
+              <table>
                 <thead>
                   <tr>
                     <th>Mã phụ phí</th>
@@ -114,19 +159,23 @@ const handleCancelClick = () => {
                       <td>{d.maNV}</td>
                       <td>{d.soLuong}</td>
                       <td>{d.tongTien}</td>
-                      <td>{d.thoiGian }</td>
+                      <td>{d.thoiGian}</td>
+                      <td id='removeData' onClick={() => handleDeleteClick(d)}><AiTwotoneDelete /></td>
+                      <div>
+                        {deleteService && (
+                          <DeleteClientForm onCancel={handleCancelDelete} onConfirm={() => handleRemoveClick('surcharges')} serviceToDelete={serviceToDelete} />
+                        )}
+                      </div>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
             <div className="button">
-                <input type="submit" value="Thoát" id="cancel-button" onClick={handleCancelClick} />
-              </div>
+              <input type="submit" value="Thoát" id="cancel-button" onClick={handleCancelClick} />
+            </div>
           </div>
-          
         </div>
-        
       </div>
     </div>
   );
